311dd24cc80b6e12c892ef4dcfa53fa20fa7d1e2
“Tali” <5lyusita@gmail.com>
2026-08-18 13:49:53 +0300

    Homework

---
+++ task.js
@@ -0,0 +1,38 @@
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if(this.marks == undefined){
+        return 0;
+    }
+    this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+    if (this.marks == undefined || this.marks.length === 0){
+        return 0;
+    }
+
+    let sum = 0;
+    let kol = 0;
+    
+    for( let mark of this.marks ){
+        sum += mark;
+    }
+    
+    return(sum/this.marks.length);
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.reason = reason;
+}
